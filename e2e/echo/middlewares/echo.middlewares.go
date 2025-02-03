b498b67a35130915cea547887f748078d88e975b
Haim Kastner <hello@haim-kastner.com>
2025-02-03 23:07:37 +0200

    Implement Echo middlewares

---
+++ e2e/echo/middlewares/echo.middlewares.go
@@ -0,0 +1,56 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gopher-fleece/gleece/e2e/assets"
+	"github.com/labstack/echo/v4"
+)
+
+func MiddlewareBeforeOperation(c echo.Context) bool {
+	c.Response().Header().Set("X-pass-before-operation", "true")
+
+	abortBeforeOperation := c.Request().Header.Get("abort-before-operation")
+	if abortBeforeOperation == "true" {
+		c.JSON(http.StatusBadRequest, map[string]string{"error": "abort-before-operation header is set to true"})
+		return false
+	}
+
+	return true
+}
+
+func MiddlewareAfterSucceedOperation(c echo.Context) bool {
+	c.Response().Header().Set("X-pass-after-succeed-operation", "true")
+
+	abortAfterSucceedOperation := c.Request().Header.Get("abort-after-operation")
+	if abortAfterSucceedOperation == "true" {
+		c.JSON(http.StatusBadRequest, map[string]string{"error": "abort-after-operation header is set to true"})
+		return false
+	}
+	return true
+}
+
+func MiddlewareOnError(c echo.Context, err error) bool {
+	c.Response().Header().Set("X-pass-on-error", "true")
+
+	abortOnError := c.Request().Header.Get("abort-on-error")
+	if abortOnError == "true" {
+		operationErr := ""
+		switch e := err.(type) {
+		case *echo.HTTPError:
+			operationErr = e.Message.(string)
+		case assets.CustomError:
+			operationErr = e.Message
+		default:
+			operationErr = err.Error()
+		}
+		c.JSON(http.StatusBadRequest, map[string]string{"error": "abort-on-error header is set to true " + operationErr})
+		return false
+	}
+	return true
+}
+
+func MiddlewareOnError2(c echo.Context, err error) bool {
+	c.Response().Header().Set("X-pass-on-error-2", "true")
+	return true
+}
