04c750577edc04f15178116eff080bca9bc86810
Haim Kastner <hello@haim-kastner.com>
2025-05-13 08:17:27 +0300

    Add missing support on openapi security schema

--- generator/swagen/swagen30/security_generator.go
+++ generator/swagen/swagen30/security_generator.go
@@ -16,6 +16,51 @@ func GenerateSecuritySpec(openapi *openapi3.T, securityConfig *[]definitions.Sec
 				Description: scheme.Description,
 			},
 		}
+		if scheme.Scheme != "" {
+			securitySchemes[scheme.SecurityName].Value.Scheme = string(scheme.Scheme)
+		}
+
+		if scheme.OpenIdConnectUrl != "" {
+			securitySchemes[scheme.SecurityName].Value.OpenIdConnectUrl = scheme.OpenIdConnectUrl
+		}
+		if scheme.Flows != nil {
+			openAPIFlows := &openapi3.OAuthFlows{}
+
+			if scheme.Flows.Implicit != nil {
+				openAPIFlows.Implicit = &openapi3.OAuthFlow{
+					AuthorizationURL: scheme.Flows.Implicit.AuthorizationURL,
+					RefreshURL:       scheme.Flows.Implicit.RefreshURL,
+					Scopes:           scheme.Flows.Implicit.Scopes,
+				}
+			}
+
+			if scheme.Flows.Password != nil {
+				openAPIFlows.Password = &openapi3.OAuthFlow{
+					TokenURL:   scheme.Flows.Password.TokenURL,
+					RefreshURL: scheme.Flows.Password.RefreshURL,
+					Scopes:     scheme.Flows.Password.Scopes,
+				}
+			}
+
+			if scheme.Flows.ClientCredentials != nil {
+				openAPIFlows.ClientCredentials = &openapi3.OAuthFlow{
+					TokenURL:   scheme.Flows.ClientCredentials.TokenURL,
+					RefreshURL: scheme.Flows.ClientCredentials.RefreshURL,
+					Scopes:     scheme.Flows.ClientCredentials.Scopes,
+				}
+			}
+
+			if scheme.Flows.AuthorizationCode != nil {
+				openAPIFlows.AuthorizationCode = &openapi3.OAuthFlow{
+					AuthorizationURL: scheme.Flows.AuthorizationCode.AuthorizationURL,
+					TokenURL:         scheme.Flows.AuthorizationCode.TokenURL,
+					RefreshURL:       scheme.Flows.AuthorizationCode.RefreshURL,
+					Scopes:           scheme.Flows.AuthorizationCode.Scopes,
+				}
+			}
+
+			securitySchemes[scheme.SecurityName].Value.Flows = openAPIFlows
+		}
 	}
 
 	openapi.Components.SecuritySchemes = securitySchemes
--- generator/swagen/swagen31/security_generator31.go
+++ generator/swagen/swagen31/security_generator31.go
@@ -9,12 +9,81 @@ import (
 func GenerateSecuritySpec(doc *v3.Document, securityConfig *[]definitions.SecuritySchemeConfig) error {
 	securitySchemes := orderedmap.New[string, *v3.SecurityScheme]()
 	for _, scheme := range *securityConfig {
-		securitySchemes.Set(scheme.SecurityName, &v3.SecurityScheme{
+		securityScheme := &v3.SecurityScheme{
 			Type:        string(scheme.Type),
 			In:          string(scheme.In),
 			Name:        scheme.FieldName,
 			Description: scheme.Description,
-		})
+		}
+
+		if scheme.Scheme != "" {
+			securityScheme.Scheme = string(scheme.Scheme)
+		}
+
+		if scheme.OpenIdConnectUrl != "" {
+			securityScheme.OpenIdConnectUrl = scheme.OpenIdConnectUrl
+		}
+
+		if scheme.Flows != nil {
+			flows := &v3.OAuthFlows{}
+
+			if scheme.Flows.Implicit != nil {
+				scopesMap := orderedmap.New[string, string]()
+				for k, v := range scheme.Flows.Implicit.Scopes {
+					scopesMap.Set(k, v)
+				}
+
+				flows.Implicit = &v3.OAuthFlow{
+					AuthorizationUrl: scheme.Flows.Implicit.AuthorizationURL,
+					RefreshUrl:       scheme.Flows.Implicit.RefreshURL,
+					Scopes:           scopesMap,
+				}
+			}
+
+			if scheme.Flows.Password != nil {
+				scopesMap := orderedmap.New[string, string]()
+				for k, v := range scheme.Flows.Password.Scopes {
+					scopesMap.Set(k, v)
+				}
+
+				flows.Password = &v3.OAuthFlow{
+					TokenUrl:   scheme.Flows.Password.TokenURL,
+					RefreshUrl: scheme.Flows.Password.RefreshURL,
+					Scopes:     scopesMap,
+				}
+			}
+
+			if scheme.Flows.ClientCredentials != nil {
+				scopesMap := orderedmap.New[string, string]()
+				for k, v := range scheme.Flows.ClientCredentials.Scopes {
+					scopesMap.Set(k, v)
+				}
+
+				flows.ClientCredentials = &v3.OAuthFlow{
+					TokenUrl:   scheme.Flows.ClientCredentials.TokenURL,
+					RefreshUrl: scheme.Flows.ClientCredentials.RefreshURL,
+					Scopes:     scopesMap,
+				}
+			}
+
+			if scheme.Flows.AuthorizationCode != nil {
+				scopesMap := orderedmap.New[string, string]()
+				for k, v := range scheme.Flows.AuthorizationCode.Scopes {
+					scopesMap.Set(k, v)
+				}
+
+				flows.AuthorizationCode = &v3.OAuthFlow{
+					AuthorizationUrl: scheme.Flows.AuthorizationCode.AuthorizationURL,
+					TokenUrl:         scheme.Flows.AuthorizationCode.TokenURL,
+					RefreshUrl:       scheme.Flows.AuthorizationCode.RefreshURL,
+					Scopes:           scopesMap,
+				}
+			}
+
+			securityScheme.Flows = flows
+		}
+
+		securitySchemes.Set(scheme.SecurityName, securityScheme)
 	}
 
 	doc.Components.SecuritySchemes = securitySchemes
